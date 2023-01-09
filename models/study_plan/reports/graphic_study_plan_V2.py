7a23786c3936835832626461a635002fa22663ce
wcabascango <99676625+wcabascango@users.noreply.github.com>
2023-01-09 17:34:52 +0000

    cambio en corequisito , s emodifico la varible heredada

--- models/study_plan/reports/graphic_study_plan_V2.py
+++ models/study_plan/reports/graphic_study_plan_V2.py
@@ -1588,7 +1588,7 @@ class credit_study_plan_report(models.TransientModel):
                             
                         for corequisite_id in itinerary_id.itinerary_subject_inherit_id.core_corequisite_ids:
                             
-                            ws2.cell(column=22,row=corequisite_count,value=corequisite_id.code+" "+corequisite_id.name)
+                            ws2.cell(column=22,row=corequisite_count,value=corequisite_id.corequisite_subject_id.code+" "+corequisite_id.corequisite_subject_id.name)
                             corequisite_count+=1
                         
                         
