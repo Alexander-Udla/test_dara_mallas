fd08aa5b8ff8ce216f2e68a6f6639758603104e0
wcabascango <99676625+wcabascango@users.noreply.github.com>
2023-02-28 16:04:40 -0500

    se agrego para descripcion del curso

--- models/subjects/subject_description.py
+++ models/subjects/subject_description.py
@@ -1,10 +1,13 @@
 from odoo import models, fields, api
-class subject_description_line(models.Model):
+class subject_description(models.Model):
     _name="dara_mallas.subject_description_line"
 
     subject_id=fields.Many2one("dara_mallas.subject")
     subject_code=fields.Char(related="subject_id.code")
-    subject_description_ids = fields.One2many("dara_mallas.subject_description",inverse_name="subject_description_id",string="Sílabos")
+    content_description=fields.Text("Descripcion mínima",track_visibility='always')
+    content_description_en=fields.Text("Minimum description",track_visibility='always')
+    period_id=fields.Many2one("dara_mallas.period","Periodo")
+    tipo = fields.Char("Tipo")
     
     def name_get(self):
         result = []
@@ -12,21 +15,7 @@ class subject_description_line(models.Model):
             result.append((rec.id,'%s - %s' % (str(rec.subject_id.name),str(rec.subject_id.code))))
         return result
 
-class subject_description(models.Model):
-    _name="dara_mallas.subject_description"
 
-    period_id=fields.Many2one("dara_mallas.period")
-    content_description=fields.Text("Descripcion mínima",track_visibility='always')
-    content_description_en=fields.Text("Minimum description",track_visibility='always')
-    state = fields.Boolean("Estado", default=True,track_visibility='always')
-
-    subject_description_id = fields.Many2one("dara_mallas.subject_description")
-
-    def name_get(self):
-        result = []
-        for rec in self:
-            result.append((rec.id,'%s' % (str(rec.period_id.name))))
-        return result
 
 
     
