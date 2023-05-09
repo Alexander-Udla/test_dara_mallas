ed22a7d170a71b637229ae13ff1ee6952dfee3bc
wcabascango <99676625+wcabascango@users.noreply.github.com>
2023-05-09 17:03:10 -0500

    cambios para traduccion de facultades en college

--- models/college/college.py
+++ models/college/college.py
@@ -4,6 +4,7 @@ class college(models.Model):
     _name="dara_mallas.college"
     
     name=fields.Char("Facultad")
+    name_en=fields.Char("College")
     name_short=fields.Char("Facultad Nombre Corto")
     code=fields.Char("Codigo")
     #dean_id = fields.Many2one("dara_mallas.dean") 
