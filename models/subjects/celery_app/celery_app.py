91138545e32a802763806ac557becbe3724e1b51
Alexander-Udla <alexander.gualotuna@udla.edu.ec>
2024-12-10 13:31:33 -0500

    Correcciòn interfaz homologaciòn

--- models/subjects/celery_app/celery_app.py
+++ models/subjects/celery_app/celery_app.py
@@ -5,7 +5,7 @@ sys.path.append('/odoo/odoo-server/odoo/addons')
 app = Celery('odoo', broker='redis://localhost:6379/0')
 app.conf.update(
     #result_backend='redis://localhost:6379/0',
-    result_backend='db+postgresql://postgres:jde.2020@localhost/dara_mallas',
+    result_backend='db+postgresql://postgres:root123@localhost/dara_mallas',
     #include=['tasks']
     task_serializer='json',
     accept_content=['json'],
